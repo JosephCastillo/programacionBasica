1d7dc6355350c6db95857d871b2923615d9b4209
Joseph Castillo <joseph.bjcr@gmail.com>
2018-11-18 18:56:49 -0500

    Primer commit ramificacion test2 - ejercicio fizzbuzz terminado

--- ProgBasica/FizzBuzz/fizzbuzz.js
+++ ProgBasica/FizzBuzz/fizzbuzz.js
@@ -1,1 +1,14 @@
-alert("fizzbuzz");
+for (var i = 1; i <= 100; i++)
+{
+  var cadena = "";
+  if(i % 3 == 0)
+  {
+    cadena += "Fizz";
+  }
+  if(i % 5 == 0)
+  {
+    cadena += "Buzz";
+  }
+  document.write(cadena || i);
+  document.write("<br/>")
+}
